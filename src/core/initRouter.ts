9c41ede2bc49b9a118c3156c663f6c23bdec8046
justcodebryan <huangjifan1819@163.com>
2023-02-19 22:05:18 +0800

    fix: fix cannot get request.body

--- src/core/initRouter.ts
+++
@@ -1,9 +0,0 @@
-import type Koa from 'koa'
-import rootRouter from '@/api'
-
-const initRouter = (app: Koa) => {
-  console.log('[server]: Init Router...')
-  app.use(rootRouter.routes()).use(rootRouter.allowedMethods())
-}
-
-export default initRouter
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,33 @@ require('module-alias/register')
 import Koa from 'koa'
 import siteConfig from './configs/siteConfig'
 import { DEFAULT_CRON_TIME, DEFAULT_PORT } from './utils/constants'
-import initRouter from './core/initRouter'
-import initMiddleware from './core/initMiddleware'
 import CurrencyService from '@/services/currency'
 
 import { scheduledJob } from './utils/schedule'
+import cors from 'koa2-cors'
+import bodyParser from 'koa-bodyparser'
+import logger from 'koa-logger'
+import { errorHandler } from './utils/errorHandler'
+import rootRouter from './api'
 
 const port = Number(siteConfig.port) || DEFAULT_PORT
 
 const app = new Koa()
 
-initMiddleware(app)
-initRouter(app)
+// Load Middleware
+app.use(cors())
+app.use(bodyParser())
+app.use(errorHandler())
+app.use(
+  logger({
+    transporter: (str) => {
+      console.log(`[http]: ${str}`)
+    },
+  })
+)
+app.on('error', console.error)
+
+app.use(rootRouter.routes()).use(rootRouter.allowedMethods())
 
 const server = app.listen(port)
 console.log(`[server]: Server started at port ${port}...`)
