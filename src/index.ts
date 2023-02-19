71875d22a1b81391ee8b11d5e0070e2f220dcfb9
justcodebryan <huangjifan1819@163.com>
2023-02-19 22:08:56 +0800

    chore: change format

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import { scheduledJob } from './utils/schedule'
 import cors from 'koa2-cors'
 import bodyParser from 'koa-bodyparser'
 import logger from 'koa-logger'
-import { errorHandler } from './utils/errorHandler'
+import errorHandler from './utils/errorHandler'
 import rootRouter from './api'
 
 const port = Number(siteConfig.port) || DEFAULT_PORT
@@ -29,13 +29,16 @@ app.use(
 )
 app.on('error', console.error)
 
+// Init Router
 app.use(rootRouter.routes()).use(rootRouter.allowedMethods())
 
+// Start server
 const server = app.listen(port)
 console.log(`[server]: Server started at port ${port}...`)
 
+// Cron Job
 export const job = scheduledJob(DEFAULT_CRON_TIME, CurrencyService.getCloudAPICurrencyData)
-// job.start()
-job.stop()
+job.start()
 
+// Export server for unit test
 export default server
